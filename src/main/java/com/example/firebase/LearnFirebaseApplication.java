dc6cdd77255c16a0f160223e654767246bc4946c
songk1992-home <zuppiy@naver.com>
2021-03-30 16:14:19 +0900

    init

---
+++ src/main/java/com/example/firebase/LearnFirebaseApplication.java
@@ -0,0 +1,13 @@
+package com.example.firebase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnFirebaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnFirebaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/firebase/LearnFirebaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.firebase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnFirebaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
